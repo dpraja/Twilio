d168abcfb50cd48bde06437429447fd81f320035
dpraja <raja.pushpanathan@gmail.com>
2018-09-21 12:51:12 +0530

    updateurl

--- app_url.py
+++ app_url.py
@@ -3,6 +3,9 @@ from flask import Flask,request, jsonify
 from flask_cors import CORS
 from Inserttwilioreservation import Inserttwilioreservation
 from Inserttwilioreservation import InsertArrivalDeparture
+from Inserttwilioreservation import Modifytwilioreservation
+from Inserttwilioreservation import Canceltwilioreservation
+from Inserttwilioreservation import Smstwilioservice
 app = Flask(__name__)
 CORS(app)
 
@@ -13,6 +16,15 @@ def reservation():
 def InsertArrivalDeparture_all():
    return InsertArrivalDeparture(request)
 
+@app.route('/Modifytwilioreservation',methods=['POST'])
+def Modifytwilioreservation_all():
+   return Modifytwilioreservation(request)
+@app.route('/Canceltwilioreservation',methods=['POST'])
+def Canceltwilioreservation_all():
+   return Canceltwilioreservation(request)
+@app.route('/Smstwilioservice',methods=['POST'])
+def Smstwilioservice_all():
+   return Smstwilioservice(request)
 if __name__ == "__main__":
   #app.run(debug=True)
   app.run(host="192.168.56.1",port=5000)
