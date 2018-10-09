3c6d36a29e5b0fb1cba31239b8a235d29fd2cd04
dpraja <raja.pushpanathan@gmail.com>
2018-10-09 13:46:33 +0530

    Update Send_OTP.py

--- Send_OTP.py
+++ Send_OTP.py
@@ -6,7 +6,9 @@ import json
 
 
 def index(request):
-    mobile = request.json['mobile']
+    country_code=request.json['country_code']
+    mobile_number = request.json['mobile_number']
+    mobile=country_code + mobile_number
     authkey_msg91 = '195833ANU0xiap5a708d1f'
     c = random.randint(0,99999999)
     print(c)
