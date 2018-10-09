b7d76a8e2b3435f57f776ead8936240ff7173850
dpraja <raja.pushpanathan@gmail.com>
2018-10-09 10:42:20 +0530

    Create verify_OTP.py

---
+++ verify_OTP.py
@@ -0,0 +1,21 @@
+from flask import Flask,request
+import urllib
+import psycopg2
+import json
+
+def indexverifyOTP(request):
+    otp_num = request.json['otp_num']
+    mobile_number = request.json['mobile_number']
+    authkey_msg91 = '195833ANU0xiap5a708d1f'
+    
+
+    url = 'https://control.msg91.com/api/verifyRequestOTP.php?authkey='+authkey_msg91+'&mobile='+mobile_number+'&otp='+otp_num
+    
+    print(url)
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+       the_page = response.read()
+       test = str(the_page)
+       test = test[2:-1]
+       test = json.loads(test)
+    return json.dumps([(test)])
