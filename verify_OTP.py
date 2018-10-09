3c8abeffa58846877e3f86d09f9d295b5fca157f
dpraja <raja.pushpanathan@gmail.com>
2018-10-09 13:52:12 +0530

    Update verify_OTP.py

--- verify_OTP.py
+++ verify_OTP.py
@@ -5,11 +5,14 @@ import json
 
 def indexverifyOTP(request):
     otp_num = request.json['otp_num']
+    country_code=request.json['country_code']
     mobile_number = request.json['mobile_number']
+    mobile=country_code + mobile_number
+    print(mobile)
     authkey_msg91 = '195833ANU0xiap5a708d1f'
     
 
-    url = 'https://control.msg91.com/api/verifyRequestOTP.php?authkey='+authkey_msg91+'&mobile='+mobile_number+'&otp='+otp_num
+    url = 'https://control.msg91.com/api/verifyRequestOTP.php?authkey='+authkey_msg91+'&mobile='+mobile+'&otp='+otp_num
     
     print(url)
     req = urllib.request.Request(url)
