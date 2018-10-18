b8fe4d25fba8d2b269744e3536d29e3c44a46459
dpraja <raja.pushpanathan@gmail.com>
2018-10-18 17:46:27 +0530

    Update botinsert.py

--- botinsert.py
+++ botinsert.py
@@ -1,10 +1,20 @@
 
 import json
+from datetime import datetime
+from pytz import timezone
 from sqlwrapperbot import gensql, dbget,dbput
 
 
 def fun(request):
    d = request.json
+   format = "%Y-%m-%d %H:%M:%S %Z%z"
+   # Current time in UTC
+   now_utc = datetime.now(timezone('UTC'))
+   print(now_utc.strftime(format))
+   # Convert to Asia/Kolkata time zone
+   now_asia = now_utc.astimezone(timezone('Asia/Kolkata'))
+   print(now_asia.strftime(format))
+   d['custom_date'] = now_asia
    sql = gensql('insert','botschema.infocuit__website_table',d)
    print(sql)
    return(json.dumps({"Return":"Success","Status":200}))
